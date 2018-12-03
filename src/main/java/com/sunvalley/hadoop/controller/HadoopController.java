99e5e59a7f68091400d57e865782041bbe21c74d
Logan zou <logan.zou@sunvalley.com.cn>
2018-12-03 15:25:08 +0800

    hdfs方法封装

--- src/main/java/com/sunvalley/hadoop/controller/HadoopController.java
+++ src/main/java/com/sunvalley/hadoop/controller/HadoopController.java
@@ -3,20 +3,12 @@ package com.sunvalley.hadoop.controller;
 import com.sunvalley.hadoop.VO.BaseReturnVO;
 import com.sunvalley.hadoop.util.HadoopUtil;
 import org.apache.commons.lang.StringUtils;
-import org.apache.hadoop.fs.*;
-import org.apache.hadoop.io.IOUtils;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.InputStream;
-import java.io.InputStreamReader;
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -40,13 +32,8 @@ public class HadoopController {
         if (StringUtils.isEmpty(path)) {
             return new BaseReturnVO("请求参数为空");
         }
-        // 文件对象
-        FileSystem fs = HadoopUtil.getFileSystem();
-        // 目标路径
-        Path newPath = new Path(path);
         // 创建空文件夹
-        boolean isOk = fs.mkdirs(newPath);
-        fs.close();
+        boolean isOk = HadoopUtil.mkdir(path);
         if (isOk) {
             return new BaseReturnVO("create dir success");
         } else {
@@ -63,21 +50,8 @@ public class HadoopController {
      */
     @PostMapping("/readPathInfo")
     public BaseReturnVO readPathInfo(@RequestParam("path") String path) throws Exception {
-        FileSystem fs = HadoopUtil.getFileSystem();
-        Path newPath = new Path(path);
-        FileStatus[] statusList = fs.listStatus(newPath);
-        List<Map<String, Object>> list = new ArrayList<>();
-        if (null != statusList && statusList.length > 0) {
-            for (FileStatus fileStatus : statusList) {
-                Map<String, Object> map = new HashMap<>();
-                map.put("filePath", fileStatus.getPath());
-                map.put("fileStatus", fileStatus.toString());
-                list.add(map);
-            }
-            return new BaseReturnVO(list);
-        } else {
-            return new BaseReturnVO("目录内容为空");
-        }
+        List<Map<String, Object>> list = HadoopUtil.readPathInfo(path);
+        return new BaseReturnVO(list);
     }
 
     /**
@@ -92,15 +66,7 @@ public class HadoopController {
         if (StringUtils.isEmpty(path) || null == file.getBytes()) {
             return new BaseReturnVO("请求参数为空");
         }
-        String fileName = file.getOriginalFilename();
-        FileSystem fs = HadoopUtil.getFileSystem();
-        // 上传时默认当前目录，后面自动拼接文件的目录
-        Path newPath = new Path(path + "/" + fileName);
-        // 打开一个输出流
-        FSDataOutputStream outputStream = fs.create(newPath);
-        outputStream.write(file.getBytes());
-        outputStream.close();
-        fs.close();
+        HadoopUtil.createFile(path, file);
         return new BaseReturnVO("create file success");
     }
 
@@ -113,24 +79,8 @@ public class HadoopController {
      */
     @PostMapping("/readFile")
     public BaseReturnVO readFile(@RequestParam("path") String path) throws Exception {
-        FileSystem fs = HadoopUtil.getFileSystem();
-        Path newPath = new Path(path);
-        FSDataInputStream inputStream = null;
-        try {
-            inputStream = fs.open(newPath);
-            // 防止中文乱码
-            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
-            String lineTxt = "";
-            StringBuffer sb = new StringBuffer();
-            while ((lineTxt = reader.readLine()) != null) {
-                System.out.println(lineTxt);
-                sb.append(lineTxt);
-            }
-            return new BaseReturnVO(sb);
-        } finally {
-            inputStream.close();
-            fs.close();
-        }
+        String targetPath = HadoopUtil.readFile(path);
+        return new BaseReturnVO(targetPath);
     }
 
     /**
@@ -145,21 +95,7 @@ public class HadoopController {
         if (StringUtils.isEmpty(path)) {
             return new BaseReturnVO("请求参数为空");
         }
-        FileSystem fs = HadoopUtil.getFileSystem();
-        Path newPath = new Path(path);
-        // 递归找到所有文件
-        RemoteIterator<LocatedFileStatus> filesList = fs.listFiles(newPath, true);
-        List<Map<String, String>> returnList = new ArrayList<>();
-        while (filesList.hasNext()) {
-            LocatedFileStatus next = filesList.next();
-            String fileName = next.getPath().getName();
-            Path filePath = next.getPath();
-            Map<String, String> map = new HashMap<>();
-            map.put("fileName", fileName);
-            map.put("filePath", filePath.toString());
-            returnList.add(map);
-        }
-        fs.close();
+        List<Map<String, String>> returnList = HadoopUtil.listFile(path);
         return new BaseReturnVO(returnList);
     }
 
@@ -176,11 +112,7 @@ public class HadoopController {
         if (StringUtils.isEmpty(oldName) || StringUtils.isEmpty(newName)) {
             return new BaseReturnVO("请求参数为空");
         }
-        FileSystem fs = HadoopUtil.getFileSystem();
-        Path oldPath = new Path(oldName);
-        Path newPath = new Path(newName);
-        boolean isOk = fs.rename(oldPath, newPath);
-        fs.close();
+        boolean isOk = HadoopUtil.renameFile(oldName, newName);
         if (isOk) {
             return new BaseReturnVO("rename file success");
         } else {
@@ -197,10 +129,7 @@ public class HadoopController {
      */
     @PostMapping("/deleteFile")
     public BaseReturnVO deleteFile(@RequestParam("path") String path) throws Exception {
-        FileSystem fs = HadoopUtil.getFileSystem();
-        Path newPath = new Path(path);
-        boolean isOk = fs.deleteOnExit(newPath);
-        fs.close();
+        boolean isOk = HadoopUtil.deleteFile(path);
         if (isOk) {
             return new BaseReturnVO("delete file success");
         } else {
@@ -218,15 +147,7 @@ public class HadoopController {
      */
     @PostMapping("/uploadFile")
     public BaseReturnVO uploadFile(@RequestParam("path") String path, @RequestParam("uploadPath") String uploadPath) throws Exception {
-        FileSystem fs = HadoopUtil.getFileSystem();
-        // 上传路径
-        Path clientPath = new Path(path);
-        // 目标路径
-        Path serverPath = new Path(uploadPath);
-
-        // 调用文件系统的文件复制方法，第一个参数是否删除原文件true为删除，默认为false
-        fs.copyFromLocalFile(false, clientPath, serverPath);
-        fs.close();
+        HadoopUtil.uploadFile(path, uploadPath);
         return new BaseReturnVO("upload file success");
     }
 
@@ -239,15 +160,7 @@ public class HadoopController {
      */
     @PostMapping("/downloadFile")
     public BaseReturnVO downloadFile(@RequestParam("path") String path, @RequestParam("downloadPath") String downloadPath) throws Exception {
-        FileSystem fs = HadoopUtil.getFileSystem();
-        // 上传路径
-        Path clientPath = new Path(path);
-        // 目标路径
-        Path serverPath = new Path(downloadPath);
-
-        // 调用文件系统的文件复制方法，第一个参数是否删除原文件true为删除，默认为false
-        fs.copyToLocalFile(false, clientPath, serverPath);
-        fs.close();
+        HadoopUtil.downloadFile(path, downloadPath);
         return new BaseReturnVO("download file success");
     }
 
@@ -260,25 +173,8 @@ public class HadoopController {
      */
     @PostMapping("/copyFile")
     public BaseReturnVO copyFile(@RequestParam("sourcePath") String sourcePath, @RequestParam("targetPath") String targetPath) throws Exception {
-        FileSystem fs = HadoopUtil.getFileSystem();
-        // 原始文件路径
-        Path oldPath = new Path(sourcePath);
-        // 目标路径
-        Path newPath = new Path(targetPath);
-
-        FSDataInputStream inputStream = null;
-        FSDataOutputStream outputStream = null;
-        try {
-            inputStream = fs.open(oldPath);
-            outputStream = fs.create(newPath);
-
-            IOUtils.copyBytes(inputStream, outputStream, 1024*1024*64,false);
-            return new BaseReturnVO("copy file success");
-        } finally {
-            inputStream.close();
-            outputStream.close();
-            fs.close();
-        }
+        HadoopUtil.copyFile(sourcePath, targetPath);
+        return new BaseReturnVO("copy file success");
     }
 }
 
--- src/main/java/com/sunvalley/hadoop/util/HadoopUtil.java
+++ src/main/java/com/sunvalley/hadoop/util/HadoopUtil.java
@@ -1,12 +1,21 @@
 package com.sunvalley.hadoop.util;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.hadoop.conf.Configuration;
-import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.*;
+import org.apache.hadoop.io.IOUtils;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
 import javax.annotation.PostConstruct;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
 import java.net.URI;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 /**
  * 类或方法的功能描述 :Hadoop工具类
@@ -21,13 +30,13 @@ public class HadoopUtil {
 
     private static String hdfsPath;
     private static String hdfsName;
+    private static final int bufferSize = 1024 * 1024 * 64;
 
     /**
      * 获取HDFS配置信息
      * @return
      */
     private static Configuration getConfiguration() {
-
         Configuration configuration = new Configuration();
         configuration.set("fs.defaultFS", hdfsPath);
         return configuration;
@@ -40,11 +49,263 @@ public class HadoopUtil {
      */
     public static FileSystem getFileSystem() throws Exception {
         // 客户端去操作hdfs时是有一个用户身份的，默认情况下hdfs客户端api会从jvm中获取一个参数作为自己的用户身份 DHADOOP_USER_NAME=hadoop
-//        FileSystem hdfs = FileSystem.get(getHdfsConfig()); //默认获取
 //        也可以在构造客户端fs对象时，通过参数传递进去
         FileSystem fileSystem = FileSystem.get(new URI(hdfsPath), getConfiguration(), hdfsName);
         return fileSystem;
     }
+
+    /**
+     * 在HDFS创建文件夹
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    public static boolean mkdir(String path) throws Exception {
+        if (StringUtils.isEmpty(path)) {
+            return false;
+        }
+        FileSystem fs = getFileSystem();
+        // 目标路径
+        Path srcPath = new Path(path);
+        boolean isOk = fs.mkdirs(srcPath);
+        fs.close();
+        return isOk;
+    }
+
+    /**
+     * 读取HDFS目录信息
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    public static List<Map<String, Object>> readPathInfo(String path) throws Exception {
+        if (StringUtils.isEmpty(path)) {
+            return null;
+        }
+        FileSystem fs = getFileSystem();
+        // 目标路径
+        Path newPath = new Path(path);
+        FileStatus[] statusList = fs.listStatus(newPath);
+        List<Map<String, Object>> list = new ArrayList<>();
+        if (null != statusList && statusList.length > 0) {
+            for (FileStatus fileStatus : statusList) {
+                Map<String, Object> map = new HashMap<>();
+                map.put("filePath", fileStatus.getPath());
+                map.put("fileStatus", fileStatus.toString());
+                list.add(map);
+            }
+            return list;
+        } else {
+            return null;
+        }
+    }
+
+    /**
+     * HDFS创建文件
+     * @param path
+     * @param file
+     * @throws Exception
+     */
+    public static void createFile(String path, MultipartFile file) throws Exception {
+        if (StringUtils.isEmpty(path) || null == file.getBytes()) {
+            return;
+        }
+        String fileName = file.getOriginalFilename();
+        FileSystem fs = getFileSystem();
+        // 上传时默认当前目录，后面自动拼接文件的目录
+        Path newPath = new Path(path + "/" + fileName);
+        // 打开一个输出流
+        FSDataOutputStream outputStream = fs.create(newPath);
+        outputStream.write(file.getBytes());
+        outputStream.close();
+        fs.close();
+    }
+
+    /**
+     * 读取HDFS文件内容
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    public static String readFile(String path) throws Exception {
+        if (StringUtils.isEmpty(path)) {
+            return null;
+        }
+        FileSystem fs = getFileSystem();
+        // 目标路径
+        Path srcPath = new Path(path);
+        FSDataInputStream inputStream = null;
+        try {
+            inputStream = fs.open(srcPath);
+            // 防止中文乱码
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+            String lineTxt = "";
+            StringBuffer sb = new StringBuffer();
+            while ((lineTxt = reader.readLine()) != null) {
+                sb.append(lineTxt);
+            }
+            return sb.toString();
+        } finally {
+            inputStream.close();
+            fs.close();
+        }
+    }
+
+    /**
+     * 读取HDFS文件列表
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    public static List<Map<String, String>> listFile(String path) throws Exception {
+        if (StringUtils.isEmpty(path)) {
+            return null;
+        }
+        FileSystem fs = getFileSystem();
+        // 目标路径
+        Path srcPath = new Path(path);
+        // 递归找到所有文件
+        RemoteIterator<LocatedFileStatus> filesList = fs.listFiles(srcPath, true);
+        List<Map<String, String>> returnList = new ArrayList<>();
+        while (filesList.hasNext()) {
+            LocatedFileStatus next = filesList.next();
+            String fileName = next.getPath().getName();
+            Path filePath = next.getPath();
+            Map<String, String> map = new HashMap<>();
+            map.put("fileName", fileName);
+            map.put("filePath", filePath.toString());
+            returnList.add(map);
+        }
+        fs.close();
+        return returnList;
+    }
+
+    /**
+     * HDFS重命名文件
+     * @param oldName
+     * @param newName
+     * @return
+     * @throws Exception
+     */
+    public static boolean renameFile(String oldName, String newName) throws Exception {
+        if (StringUtils.isEmpty(oldName) || StringUtils.isEmpty(newName)) {
+            return false;
+        }
+        FileSystem fs = getFileSystem();
+        // 原文件目标路径
+        Path oldPath = new Path(oldName);
+        // 重命名目标路径
+        Path newPath = new Path(newName);
+        boolean isOk = fs.rename(oldPath, newPath);
+        fs.close();
+        return isOk;
+    }
+
+    /**
+     * 删除HDFS文件
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    public static boolean deleteFile(String path) throws Exception {
+        if (StringUtils.isEmpty(path)) {
+            return false;
+        }
+        FileSystem fs = getFileSystem();
+        Path srcPath = new Path(path);
+        boolean isOk = fs.deleteOnExit(srcPath);
+        fs.close();
+        return isOk;
+    }
+
+    /**
+     * 上传HDFS文件
+     * @param path
+     * @param uploadPath
+     * @throws Exception
+     */
+    public static void uploadFile(String path, String uploadPath) throws Exception {
+        if (StringUtils.isEmpty(path) || StringUtils.isEmpty(uploadPath)) {
+            return;
+        }
+        FileSystem fs = getFileSystem();
+        // 上传路径
+        Path clientPath = new Path(path);
+        // 目标路径
+        Path serverPath = new Path(uploadPath);
+
+        // 调用文件系统的文件复制方法，第一个参数是否删除原文件true为删除，默认为false
+        fs.copyFromLocalFile(false, clientPath, serverPath);
+        fs.close();
+    }
+
+    /**
+     * 下载HDFS文件
+     * @param path
+     * @param downloadPath
+     * @throws Exception
+     */
+    public static void downloadFile(String path, String downloadPath) throws Exception {
+        if (StringUtils.isEmpty(path) || StringUtils.isEmpty(downloadPath)) {
+            return;
+        }
+        FileSystem fs = getFileSystem();
+        // 上传路径
+        Path clientPath = new Path(path);
+        // 目标路径
+        Path serverPath = new Path(downloadPath);
+
+        // 调用文件系统的文件复制方法，第一个参数是否删除原文件true为删除，默认为false
+        fs.copyToLocalFile(false, clientPath, serverPath);
+        fs.close();
+    }
+
+    /**
+     * 判断HDFS文件是否存在
+     * @param path
+     * @return
+     * @throws Exception
+     */
+    public static boolean existFile(String path) throws Exception {
+        if (StringUtils.isEmpty(path)) {
+            return false;
+        }
+        FileSystem fs = getFileSystem();
+        Path srcPath = new Path(path);
+        boolean isExists = fs.exists(srcPath);
+        return isExists;
+    }
+
+    /**
+     * HDFS文件复制
+     * @param sourcePath
+     * @param targetPath
+     * @throws Exception
+     */
+    public static void copyFile(String sourcePath, String targetPath) throws Exception {
+        if (StringUtils.isEmpty(sourcePath) || StringUtils.isEmpty(targetPath)) {
+            return;
+        }
+        FileSystem fs = getFileSystem();
+        // 原始文件路径
+        Path oldPath = new Path(sourcePath);
+        // 目标路径
+        Path newPath = new Path(targetPath);
+
+        FSDataInputStream inputStream = null;
+        FSDataOutputStream outputStream = null;
+        try {
+            inputStream = fs.open(oldPath);
+            outputStream = fs.create(newPath);
+
+            IOUtils.copyBytes(inputStream, outputStream, bufferSize,false);
+        } finally {
+            inputStream.close();
+            outputStream.close();
+            fs.close();
+        }
+    }
+
 
     @PostConstruct
     public void getPath() {
